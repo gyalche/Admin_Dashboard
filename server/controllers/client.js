51ca3244200a4554da196c8b75b0f0edb798bf2c
Dawa <sherpadawa01@outlook.com>
2022-12-23 20:37:11 +0545

    geographical map

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -3,6 +3,8 @@ import ProductStat from '../models/ProductStat.js';
 import Transaction from '../models/Transaction.js';
 import user from '../models/User.js';
 // import user from './models/user.js';
+import getCountryIso3 from 'country-iso-2-to-3';
+
 export const getProducts = async (req, res, next) => {
   try {
     const product = await Product.find();
@@ -66,6 +68,30 @@ export const getTransactions = async (req, res) => {
       transactions,
       total,
     });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const getUser = await user.find();
+    const mappedLocations = getUser.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
