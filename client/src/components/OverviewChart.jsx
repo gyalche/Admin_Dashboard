6bdb6d005babf9cfc0051d8a6ed15b8753bc1372
Dawa <sherpadawa01@outlook.com>
2022-12-25 20:44:07 +0545

    workin on line graph

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -43,10 +43,117 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       },
       { sales: 0, units: 0 }
     );
-  }, [data]);
+    return [[totalSalesLine], [totalUnitsLine]];
+  }, [data]); //esline-disable-line react-hooks/exhaustive-deps
 
   if (!data || isLoading) return 'Loading...';
-  return <div>OverviewChart</div>;
+  return (
+    <ResponsiveLine
+      data={view === 'sales' ? totalSalesLine : totalUnitsLine}
+      theme={{
+        axis: {
+          domain: {
+            line: {
+              stroke: theme.palette.secondary[200],
+            },
+          },
+          legend: { text: { fill: theme.palette.secondary[200] } },
+        },
+        ticks: {
+          line: {
+            stroke: theme.palette.secondary[200],
+            strokeWidth: 1,
+          },
+          text: { fill: theme.palette.secondary[200] },
+        },
+        lengends: {
+          text: {
+            fill: theme.palette.secondary[200],
+          },
+        },
+        tooltip: {
+          container: {
+            color: theme.palette.primary.main,
+          },
+        },
+      }}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 'auto',
+        max: 'auto',
+        stacked: false,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      curve="catmullRom"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        format: (v) => {
+          if (isDashboard) return v.slice(0, 3);
+          return v;
+        },
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard ? '' : 'Month',
+        legendOffset: 36,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard
+          ? ''
+          : `Total ${view === 'sales' ? 'Revenue' : 'Units'} for Year `,
+        legendOffset: -60,
+        legendPosition: 'middle',
+      }}
+      enableGridX={false}
+      enableGridY={false}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={
+        !isDashboard
+          ? [
+              {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 30,
+                translateY: -40,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemBackground: 'rgba(0, 0, 0, .03)',
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]
+          : undefined
+      }
+    />
+  );
 };
 
 export default OverviewChart;
