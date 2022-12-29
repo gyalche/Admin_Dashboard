95c507cdbd1fe3acc998a816c6e25e9e5c43fbf8
Dawa <sherpadawa01@outlook.com>
2022-12-29 19:55:22 +0545

    filtered slaes transactions

--- server/seeder.js
+++ server/seeder.js
@@ -6,12 +6,14 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from './data/index.js';
 
 //config;
@@ -33,7 +35,10 @@ const importData = async () => {
     // await ProductStat.insertMany(dataProductStat);
 
     //insert into overallStat;
-    await OverallStat.insertMany(dataOverallStat);
+    // await OverallStat.insertMany(dataOverallStat);
+
+    //insert into affiliate stat;
+    await AffiliateStat.insertMany(dataAffiliateStat);
 
     console.log(`All items added`);
     process.exit();
